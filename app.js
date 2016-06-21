67ab3c5ec4c1d173bef07ed54d7c8f19b350d1cc
Gabe Escobedo <escobedo.gabe@yahoo.com>
2016-06-21 11:04:57 -0500

    Debugging JavaScript

---
+++ app.js
@@ -0,0 +1,12 @@
+/*
+    This code adds the values of
+    two variables and alerts the results.
+*/
+
+var firstNum = 0; // first variable
+var secondNum = 12; // second variable
+var result = firstNum + secondNum; // calculate the sum
+// Alert the result
+console.log('result=' + result);
+debugger;
+alert(result);
